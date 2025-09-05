62f88295cfa3d3ef81388f79e3c8ae8569763c32
juanengml <juanengml@gmail.com>
2025-09-05 22:26:44 +0000

    vamos lá

--- mine-tracker/src/mine_tracker/pipelines/mine/pipeline.py
+++ mine-tracker/src/mine_tracker/pipelines/mine/pipeline.py
@@ -1,11 +1,13 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from mine_tracker.pipelines.mine.nodes import carregar_dados, gerar_features, carregar_dados_ultimas_4h # noqa
+from mine_tracker.pipelines.mine.nodes import carregar_dados # noqa
+from mine_tracker.pipelines.mine.nodes import gerar_features # noqa
+from mine_tracker.pipelines.mine.nodes import carregar_dados_ultimas_4h # noqa
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
             func=carregar_dados,
-            inputs=None,   # <— usa o dataset do catálogo
+            inputs=None,
             outputs="minecraft_servidores_raw",
             name="coleta_mine_node",
         ),
